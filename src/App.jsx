e151534196d34ae0ab337850fb766d5e1098fab5
Mhd-Muzammil <zamil627810@gmail.com>
2026-03-02 14:35:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function TestTailwind() {
+  return (
+    <div className="h-screen bg-black flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-teal-400">
+        Tailwind is Working 🚀
+      </h1>
+    </div>
+  );
+}
